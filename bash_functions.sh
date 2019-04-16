2e1245131deeb3b548770734069679cae6f614f2
Dmitry Obukhov <Dmitry.Obukhov@gmail.com>
2019-04-16 12:52:10 -0700

    "Moved to /opt/shrec"

--- bash_functions.sh
+++ bash_functions.sh
@@ -1,5 +1,5 @@
 # Integration:
-# 1. git clone https://github.com/DmitryObukhov/shrec.git ~/shrec
+# 1. git clone https://github.com/DmitryObukhov/shrec.git /opt/shrec
 # 2. add to ~/.bashrc the code below
 #
 # MY_EMAIL=xxx
@@ -10,15 +10,15 @@
 # DEFREPO=xxx
 # BITBUCKET_USER=xxx
 # BITBUCKET_PASSWORD=xxx
-# source ~/shrec/bash_functions.sh
+# source /opt/shrec/bash_functions.sh
 
-PATH=$PATH:~/shrec
+PATH=$PATH:/opt/shrec
 
-source ~/shrec/bash_functions/git.sh
-source ~/shrec/bash_functions/docker.sh
-source ~/shrec/bash_functions/apt.sh
-source ~/shrec/bash_functions/shrec.sh
-source ~/shrec/bash_functions/multimedia.sh
+source /opt/shrec/bash_functions/git.sh
+source /opt/shrec/bash_functions/docker.sh
+source /opt/shrec/bash_functions/apt.sh
+source /opt/shrec/bash_functions/shrec.sh
+source /opt/shrec/bash_functions/multimedia.sh
 
 # cat user.json | grep default_repo | awk '{print $2}' | tr -d '"'
 
